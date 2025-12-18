9ca994457a0ea4e2381d2bb60d0ed7010bab8811
Dominic Sisneros <dsisnero@gmail.com>
2025-12-18 14:51:04 -0700

    Clean up temporary test files

--- test_exact.cr
+++
@@ -1,7 +0,0 @@
-require "./src/nucleoc"
-
-matcher = Nucleoc::Matcher.new
-score = matcher.exact_match("hello", "hello")
-puts "Exact match 'hello' in 'hello': #{score}"
-puts "Expected: 140"
-puts "Got: #{score.inspect}"
